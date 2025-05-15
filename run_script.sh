26092e659e971ba804dd96af5c34bad3ec3a5056
loleht <loore.lehtmets@gmail.com>
2025-05-15 12:47:30 +0300

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -18,11 +18,5 @@ cd ~
 
 # Run the Python training script
 
-python final_finetune_train.py --config config.yaml --data 1004_final_finetune_train.json
-# python final_finetune_test.py
-
-# python pref_alignment_train.py --config config.yaml --data pref_alignment_train_all.json
-# python grading_test_filtered.py
-# python grading_train.py --config config.yaml --data grading_train.json
-# python new_new_grading_test.py
-# python new_new_grading_train.py --config config.yaml --data uus_uus_grading_train.json
+python finetune_train.py --config config.yaml --data FT-13k_train.json
+# python final_test.py
